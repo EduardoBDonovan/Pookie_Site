442edfbbbc630ccd582db4fe7ee0000850dd3cfc
EduardoBDonovan <eddiemb26@gmail.com>
2025-02-14 11:36:46 -0800

    Gallery images

--- src/pages/PookieHome.jsx
+++ src/pages/PookieHome.jsx
@@ -3,17 +3,26 @@ import "../styles/PookieHome.css";
 import { FaHeart } from "react-icons/fa";
 
 function PookieHome() {
+  const imageModules = import.meta.glob("../images/*.{png,jpg,jpeg,svg}", {
+    eager: true,
+  });
+
+  // Create an array of image URLs
+  const images = Object.values(imageModules).map((mod) => mod.default);
   return (
     <div className="pookiePage">
       <div className="pageHeader">
         <FaHeart />
-        <i className="fa-solid fa-heart"></i>
         <h1>
           Happy Valentines Day! <br />I love you Nugget!
         </h1>
         <FaHeart />
       </div>
-      <div className="image-gallery"></div>
+      <div className="image-gallery">
+        {images.map((image, index) => (
+          <img key={index} src={image} alt={`Gallery image ${index + 1}`} />
+        ))}
+      </div>
     </div>
   );
 }
